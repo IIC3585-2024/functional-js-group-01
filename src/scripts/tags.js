0a29f2e13133373c13e2932b3db811420e5eebbd
carmebe <carmebe@uc.cl>
2024-03-18 20:32:18 -0300

    feat: basic blockquotes

--- src/scripts/tags.js
+++ src/scripts/tags.js
@@ -7,18 +7,26 @@ const mdToHTML = {
   '####': { open_tag: '<h4>', close_tag: '</h4>' },
   '#####': { open_tag: '<h5>', close_tag: '</h5>' },
   '######': { open_tag: '<h6>', close_tag: '</h6>' },
+
   '': { open_tag: '<p>', close_tag: '</p>' },
+
   '*': { open_tag: '<em>', close_tag: '</em>' },
   '_': { open_tag: '<em>', close_tag: '</em>' },
+
   '**': { open_tag: '<strong>', close_tag: '</strong>' },
   '__': { open_tag: '<strong>', close_tag: '</strong>' },
+
   '***': { open_tag: '<em><strong>', close_tag: '</strong></em>' },
   '___': { open_tag: '<em><strong>', close_tag: '</strong></em>' },
   '__*': { open_tag: '<em><strong>', close_tag: '</strong></em>' },
   '**_': { open_tag: '<em><strong>', close_tag: '</strong></em>' },
+
   '`': { open_tag: '<code>', close_tag: '</code>' },
   '``': { open_tag: '<code>', close_tag: '</code>' },
+
   '  \n': { open_tag: '', close_tag: '</br>' },
+
+  '>': { open_tag: '<blockquote>', close_tag: '</blockquote>' },
 };
 
 export default mdToHTML;
