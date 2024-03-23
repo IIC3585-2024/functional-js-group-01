677f02ca610542bf796ad78e9475019673f81be9
leoncente <leon.sergio00@hotmail.com>
2024-03-23 16:51:44 -0300

    fix: code blocks

--- src/scripts/parsers/codes/codeBlocks.js
+++ src/scripts/parsers/codes/codeBlocks.js
@@ -1,7 +1,7 @@
 const parseCodeBlocks = (text) => {
   let newText = text;
   newText = newText
-    .replace(/^ {4}(?![-*+0123456789.])([^-*+0123456789.]*?\n)/gm, '<pre><np>$1</np></pre>\n')
+    .replace(/^ {4}(?![-*+0123456789.])([^-*+0123456789.]*?(?:\n|$))/gm, '<pre><np>$1</np></pre>\n')
     .replace(/<\/pre>\n<pre>/g, '\n');
 
   const fragments = newText.match(/(?<=<pre>)([^]+?)(?=<\/pre>)/gm);
